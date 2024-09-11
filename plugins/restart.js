dacf79307c446c591f126885106c4536e5418fd5
senulruwaneka <senulruwaneka@gmail.com>
2024-09-11 10:23:35 +0530

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -12,7 +12,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 const {exec} = require("child_process")
-reply("𝐑𝐄𝐒𝐓𝐀𝐑𝐓𝐈𝐍𝐆...♻️")
+reply("*Restarting...*")
 await sleep(1500)
 exec("pm2 restart all")
 }catch(e){
