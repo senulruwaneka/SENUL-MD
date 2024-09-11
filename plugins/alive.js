b71501221788575727f2c6e6242fe8caf84f6b01
senulruwaneka <senulruwaneka@gmail.com>
2024-09-11 09:19:35 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -11,7 +11,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 let des = `*👋 Hello ${pushname}*👋 *HELLO, I AM SENUL-MD I AM ALIVE NOW !*`
-return await conn.sendMessage(from,{image: {url: `https://i.imgur.com/qtFG9Y6.jpeg`},caption: des},{quoted: mek})
+return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: des},{quoted: mek})
 }catch(e){
 console.log(e)
 reply(`${e}`)
