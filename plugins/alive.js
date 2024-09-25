5be991da6028e10835b92e48bcef4dd20e2a8b89
senulruwaneka <senulruwaneka@gmail.com>
2024-09-25 17:54:39 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -10,7 +10,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let des = `*👋 HELLO ${pushname}*\n\n*I'M SENUL-MD WHATSAPP BOT*\n\n𝗿𝗲𝗽𝗼 𝗹𝗶𝗻𝗸 🦹👇https://github.com/senulruwaneka/SENUL-MD\n\n 𝗛𝗔𝗩𝗘 𝗔 𝗡𝗜𝗖𝗘 𝗗𝗔𝗬 💗🌝\n\n> *© ᴘᴏᴡᴇʀ ʙʏ ꜱᴇɴᴜʟ-ᴍᴅ*`
+let des = `*👋 HELLO ${pushname}*\n\n*I'M SENUL-MD WHATSAPP BOT*\n\n𝗿𝗲𝗽𝗼 𝗹𝗶𝗻𝗸 🦹👇https://github.com/senulruwaneka/SENUL-MD\n\n 𝗛𝗔𝗩𝗘 𝗔 𝗡𝗜𝗖𝗘 𝗗𝗔𝗬 💗🦹\n\n> *© ᴘᴏᴡᴇʀ ʙʏ ꜱᴇɴᴜʟ-ᴍᴅ*`
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: des},{quoted: mek})
 }catch(e){
 console.log(e)
