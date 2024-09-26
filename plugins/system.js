ad07b6e4e8038babbbc2b969695f34e7b8aecafb
Navinofc44 <Pahasarasanujja@gmail.com>
2024-09-26 20:27:44 +0530

    Create system.js

---
+++ plugins/system.js
@@ -0,0 +1,29 @@
+const {cmd , commands} = require('../command')
+const os = require("os")
+const {runtime} = require('../lib/functions')
+
+cmd({
+    pattern: "system",
+    alias: ["status","botinfo"],
+    desc: "check up time",
+    category: "main",
+    react: "📟",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{ 
+
+let status =`┌────────────────────
+    *├ ⏰ Runtime:-  ${runtime(process.uptime())}*    
+    *├ 📟 Ram usage:- ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB*
+    *├ ⚙️ Platform:- ${os.hostname()}*
+    *├ 👨‍💻 Owner:- Senul*   
+    *├ 👾 Version:- 1.0.0*
+    └───────────────────────
+    *©ᴘᴏᴡᴇʀ ʙʏ ꜱᴇɴᴜʟ-ᴍᴅッ*`
+return reply(`${status}`)
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
