f681907df1d4fc6af783812a11bdf805012bfd63
Navinofc44 <Pahasarasanujja@gmail.com>
2024-09-28 18:49:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,6 +41,10 @@ const port = process.env.PORT || 8000;
 //=============================================
 
 async function connectToWA() {
+//============connect mongodb=================
+const connectDB = require('./lib/mongodb')
+connectDB();
+//=====================================
 console.log("*Connecting SENUL-MD BOT ✅...*");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
